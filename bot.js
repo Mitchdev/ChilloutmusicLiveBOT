2291e1724c39ed4a0f6c747bd697360a64d70710
Mitch <admin@mitchdev.net>
2018-04-26 18:40:45 +1200

    Added firebase

--- bot.js
+++ bot.js
@@ -1,8 +1,34 @@
 const Discord = require('discord.js');
+const firebase = require('firebase-admin');
+const primary = firebase.initializeApp({
+  apiKey: process.env.FIREBASE_KEY1,
+  authDomain: "chilloutmusiclive-a8271.firebaseapp.com",
+  databaseURL: "https://chilloutmusiclive-a8271.firebaseio.com",
+  projectId: "chilloutmusiclive-a8271",
+  storageBucket: "chilloutmusiclive-a8271.appspot.com",
+  messagingSenderId: "732098226532"
+}, 'primary');
+const secondary = firebase.initializeApp({
+  apiKey: process.env.FIREBASE_KEY2,
+  authDomain: "chilloutmusiclive-a8271.firebaseapp.com",
+  databaseURL: "https://chilloutmusiclive-a8271.firebaseio.com",
+  projectId: "chilloutmusiclive-a8271",
+  storageBucket: "chilloutmusiclive-a8271.appspot.com",
+  messagingSenderId: "1020325985374"
+}, 'secondary');
 const client = new Discord.Client();
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setPresence({game: { name: 'Jon Kuwada	- Cherry Cola' }, status: 'idle' }).then(console.log).catch(console.error);
+    console.log(`Bot online!`);
+    primary.firestore().collection('options').doc('settings').get().then(doc => {
+      if (doc.exists) {
+        console.log('Document data:', doc.data().song);
+      } else {
+        console.log('No such document!');
+      }
+    }).catch(err => {
+      console.log('Error getting document', err);
+    });
+    client.user.setPresence({game: { name: 'Jon Kuwada - Cherry Cola' }, status: 'idle' }).then(console.log).catch(console.error);
 });
 
 client.on('message', message => {

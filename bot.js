af2ad5b8a13fe751ea3c7de432988992f939c18d
Mitch <admin@mitchdev.net>
2018-04-26 18:23:52 +1200

    Status message

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({game: { name: 'Jon Kuwada	- Cherry Cola' }, status: 'idle' }).then(console.log).catch(console.error);
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
+    if (message.content == '!ping') {
     	message.reply('pong!');
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

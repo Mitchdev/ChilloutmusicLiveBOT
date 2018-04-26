273090852545175f49f56524b8062d82dafb3629
Mitch <admin@mitchdev.net>
2018-04-26 19:12:41 +1200

    Commit

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   credential: firebase.credential.cert({
     projectId: 'mdhomepage-74632',
     clientEmail: 'firebase-adminsdk-ynfwk@mdhomepage-74632.iam.gserviceaccount.com',
-    privateKey: process.env.SERVICE_KEY
+    privateKey: '-----BEGIN PRIVATE KEY-----\n317d08f61da448dfdf032b6883e9d75e1e4acc11\n-----END PRIVATE KEY-----\n'
   }),
   databaseURL: 'https://mdhomepage-74632.firebaseio.com'
 });

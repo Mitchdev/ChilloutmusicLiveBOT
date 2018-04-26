6d20c846bac35d9c26a2f8a923b2c3c8fb2f8d57
Mitch <admin@mitchdev.net>
2018-04-26 18:50:52 +1200

    Fixed

--- bot.js
+++ bot.js
@@ -1,40 +1,17 @@
 const Discord = require('discord.js');
 const firebase = require('firebase-admin');
-const primary = firebase.initializeApp({
-  apiKey: process.env.FIREBASE_KEY1,
-  authDomain: "chilloutmusiclive-a8271.firebaseapp.com",
-  databaseURL: "https://chilloutmusiclive-a8271.firebaseio.com",
-  projectId: "chilloutmusiclive-a8271",
-  storageBucket: "chilloutmusiclive-a8271.appspot.com",
-  messagingSenderId: "732098226532"
-}, 'primary');
-const secondary = firebase.initializeApp({
-  apiKey: process.env.FIREBASE_KEY2,
-  authDomain: "chilloutmusiclive-a8271.firebaseapp.com",
-  databaseURL: "https://chilloutmusiclive-a8271.firebaseio.com",
-  projectId: "chilloutmusiclive-a8271",
-  storageBucket: "chilloutmusiclive-a8271.appspot.com",
-  messagingSenderId: "1020325985374"
-}, 'secondary');
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_URL);
 const client = new Discord.Client();
+firebase.initializeApp(serviceAccount);
 client.on('ready', () => {
-    console.log(`Bot online!`);
-    primary.firestore().collection('options').doc('settings').get().then(doc => {
-      if (doc.exists) {
-        console.log('Document data:', doc.data().song);
-      } else {
-        console.log('No such document!');
-      }
-    }).catch(err => {
-      console.log('Error getting document', err);
-    });
+    firebase.firestore().collection('options').doc('settings').get().then(doc => {
+      if (doc.exists) {console.log('Document data:', doc.data().song);} else {console.log('No such document!');}
+    }).catch(err => {console.log('Error getting document', err);});
     client.user.setPresence({game: { name: 'Jon Kuwada - Cherry Cola' }, status: 'idle' }).then(console.log).catch(console.error);
 });
 
 client.on('message', message => {
-    if (message.content == '!ping') {
-    	message.reply('pong!');
-  	}
+  console.log(message.content);
 });
 
 client.login(process.env.BOT_TOKEN);
